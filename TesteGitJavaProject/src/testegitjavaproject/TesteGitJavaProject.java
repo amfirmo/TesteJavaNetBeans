3c85c20a26ed1c431d425c68e0f983955f772d63
71321934 <71321934@090482-P33S07.macklabs.br>
2013-04-22 21:16:04 -0300

    Teste de commit 2 wesley_firmo

--- TesteGitJavaProject/src/testegitjavaproject/TesteGitJavaProject.java
+++ TesteGitJavaProject/src/testegitjavaproject/TesteGitJavaProject.java
@@ -14,6 +14,6 @@ public class TesteGitJavaProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("TESTE 2");
+        System.out.println("TESTE 3");
     }
 }
