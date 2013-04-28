46a70193ba4d8fd23c8c4ef0769e4ba82a7208ad
amfirmo <Andre@amf-mapf>
2013-04-28 09:08:14 -0300

    TESTE FINAL DE PUSH COM COMMIT LOCAL

--- TesteGitJavaProject/src/testegitjavaproject/TesteGitJavaProject.java
+++ TesteGitJavaProject/src/testegitjavaproject/TesteGitJavaProject.java
@@ -14,7 +14,7 @@ public class TesteGitJavaProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("TESTE 4");
-        System.out.println("AQUI E UM TESTE DE MUDANCA");
+        System.out.println("TESTE FINAL DE PUSH COM COMMIT LOCAL");
+        
     }
 }
