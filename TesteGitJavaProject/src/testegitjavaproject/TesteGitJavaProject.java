596faeef40a05263bfa7450e654ab6b9014e4e39
André M. Firmo <amirandafirmo@gmail.com>
2013-04-28 09:09:27 -0300

    Update TesteGitJavaProject.java

--- TesteGitJavaProject/src/testegitjavaproject/TesteGitJavaProject.java
+++ TesteGitJavaProject/src/testegitjavaproject/TesteGitJavaProject.java
@@ -14,7 +14,7 @@ public class TesteGitJavaProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("TESTE FINAL DE PUSH COM COMMIT LOCAL");
+        System.out.println("TESTE FINAL DE COMMIT NO SERVIDOR E DEPOIS SYNCRO NO LOCAL");
         
     }
 }
